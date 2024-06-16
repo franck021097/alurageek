0be83a71c87766a6a277114c9264585ffcae3af3
Francisco C <franck_021097@hotmail.com>
2024-06-15 19:24:12 -0600

    module borrar productos

--- alurageek/js/borrarProducto.js
+++ alurageek/js/borrarProducto.js
@@ -1,54 +1,12 @@
 import { conectaAPI } from "./conectaAPI.js"
+import { mostrar } from "./mostrarProductos.js"
 
 
+const contenedorProductos = document.querySelector("[data-container-productos]");
 
-window.addEventListener("load", () => {
-    obtenerBotones();
+contenedorProductos.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(event.target.id);
+    conectaAPI.borrarProducto(event.target.id);
+    mostrar.mostrarProductos();
 });
-
-async function obtenerBotones(obtener){
-    const botonesBorrar = document.querySelectorAll(".borrar-producto");
-
-    if(obtener){
-        botonesBorrar.forEach((button) => {
-            button.onclick = () => {
-                console.log(button);
-                conectaAPI.borrarProducto(button.id);
-            };
-        });
-
-        const containerProductos = document.querySelector("[data-container-productos]");
-        const productos = await conectaAPI.listaProductos();
-    
-        let listaProductos = "";
-    
-        containerProductos.innerHTML = listaProductos;  
-    
-        productos.forEach(element => {
-    
-        listaProductos += 
-        `
-            <div class="producto">
-                                    <div>
-                                        <img src="${element.imagen}" alt="producto" class="img-producto">
-                                    </div>
-                    
-                                    <div class="info-producto">
-                                        <p>${element.nombre_producto}</p>
-                                        <div>
-                                            <p class="precio-producto">$${element.precio}</p>
-                                            <img src="./assets/trash.png" alt="borrar producto" class="borrar-producto" id="${element.id}">
-                                        </div>
-                                    </div>
-                                </div>
-            `
-            }); 
-    
-        containerProductos.innerHTML = listaProductos;  
-    }
-
-}
-
-window.addEventListener("click", () => {obtenerBotones(true)});
-
-
--- alurageek/js/mostrarProductos.js
+++ alurageek/js/mostrarProductos.js
@@ -1,35 +1,31 @@
 import { conectaAPI } from "./conectaAPI.js"
 
-
-const containerProductos = document.querySelector("[data-container-productos]");
-const productos = await conectaAPI.listaProductos();
-
-let listaProductos = "";
-
-//containerProductos.innerHTML = listaProductos;  
-
-productos.forEach(element => {
-
-listaProductos += 
-`
-    <div class="producto">
-                            <div>
-                                <img src="${element.imagen}" alt="producto" class="img-producto">
-                            </div>
-            
-                            <div class="info-producto">
-                                <p>${element.nombre_producto}</p>
-                                <div>
-                                    <p class="precio-producto">$${element.precio}</p>
-                                    <img src="./assets/trash.png" alt="borrar producto" class="borrar-producto" id="${element.id}">
+async function mostrarProductos(){
+    const containerProductos = document.querySelector("[data-container-productos]");
+    const productos = await conectaAPI.listaProductos();
+    let listaProductos = "";
+    containerProductos.innerHTML = listaProductos;
+    productos.forEach(element => {
+        listaProductos += 
+        `
+            <div class="producto">
+                                    <div>
+                                        <img src="${element.imagen}" alt="producto" class="img-producto">
+                                    </div>
+                    
+                                    <div class="info-producto">
+                                        <p>${element.nombre_producto}</p>
+                                        <div>
+                                            <p class="precio-producto">$${element.precio}</p>
+                                                <img src="./assets/trash.png" alt="borrar producto" id="${element.id}">
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-    `
-    }); 
-
-containerProductos.innerHTML = listaProductos;  
-
-
-
-
+            `
+            }); 
+        containerProductos.innerHTML = listaProductos;
+        const elementos = document.querySelectorAll(".borrar-producto");
+        return elementos;
+}
+window.addEventListener("load", () => {mostrarProductos();});
+export const mostrar = {mostrarProductos}
